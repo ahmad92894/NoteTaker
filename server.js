60a34fb53d48c347cfb2e7ca8374a9b2178032ba
ahmad92894 <ahmad92894@gmail.com>
2023-04-01 13:43:46 -0500

    bcs is the best

--- server.js
+++ server.js
@@ -9,11 +9,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
-app.get('api/notes', (req, res) => {
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+app.get('/api/notes', (req, res) => {
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) throw err;
         res.json(JSON.parse(data));
@@ -33,18 +35,7 @@ app.post('/api/notes', (req, res) => {
     });
 });
     
-    app.delete('./db/db.json', (req, res) => {
-        fs.readFile('./db/db.json', 'utf8', (err, data) => {
-            if (err) throw err;
-            const notes = JSON.parse(data);
-            const noteId = req.params.id;
-            const newNotes = notes.filter((note) => note.id !== noteId);
-            fs.writeFile('./db/db.json', JSON.stringify(newNotes), (err) => {
-                if (err) throw err;
-                res.json(newNotes);
-            });
-        });
-    });
+    
     
 
 app.listen(PORT, () => 
