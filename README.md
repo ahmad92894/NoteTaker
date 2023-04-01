c68676a6ab9a1d885809df1a66b4e43d6ab41237
ahmad92894 <ahmad92894@gmail.com>
2023-04-01 13:11:51 -0500

    grrrrrr

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 Starter code given
 worked in a group
 pushed to HEROKU
-code was ruined by BCS 👏
+used class assignments as a guide/resource for this assignment
 HEROKU: https://secret-scrubland-52635.herokuapp.com/
 url: https://github.com/ahmad92894/NoteTaker
+###### coded by Ahmad Bustami ######
--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const fs = require('fs');
-const apiRoutes = require('./routes/apiroutes');  
-// const htmlRoutes = require('./routes/htmlroutes');
-//const router = express.Router();
+const path = require('path');
+const uuid = require('./helpers/uuid');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -10,11 +9,44 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// app.use('/', htmlRoutes);
- app.use('/api', apiRoutes);
 
-
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+});
+app.get('api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        res.json(JSON.parse(data));
+    });
+});
+app.post('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+        const newNote = req.body;
+        newNote.id = uuid();
+        notes.push(newNote);
+        fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
+            if (err) throw err;
+            res.json(newNote);
+        });
+    });
+});
+    
+    app.delete('./db/db.json', (req, res) => {
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) throw err;
+            const notes = JSON.parse(data);
+            const noteId = req.params.id;
+            const newNotes = notes.filter((note) => note.id !== noteId);
+            fs.writeFile('./db/db.json', JSON.stringify(newNotes), (err) => {
+                if (err) throw err;
+                res.json(newNotes);
+            });
+        });
+    });
+    
 
 app.listen(PORT, () => 
-    console.log(`App listening on PORT: ${PORT}`));
+console.log(`App listening at http://localhost:${PORT} 🚀`));
 
